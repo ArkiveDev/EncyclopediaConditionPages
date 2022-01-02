2a3c84da9897ff8061df322c9be4adb81f6d0251
arkiveDev <arkiveDev@hotmail.com>
2022-01-02 04:31:20 -0500

    Added localization. Added pages for most Conditions

--- EncyclopediaExpanded/Localization.cs
+++ EncyclopediaExpanded/Localization.cs
@@ -1,32 +1,37 @@
 ﻿using Kingmaker.Blueprints.JsonSystem;
 using Kingmaker.Localization;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static UnityModManagerNet.UnityModManager;
 
 namespace EncyclopediaExpanded
 {
     public static class Localization
     {
-        static readonly (string , string )[] enGBConditionStringsTuples =
-        {
-            ("ShakenTitle","Shaken Condition"),
-            ("ShakenDescription","A shaken character takes a –2 penalty on attack rolls, saving throws, skill checks, and ability checks." )
-
-        };
+        public static ModEntry modEntry;
 
         public static void AddEnglishStrings() 
         {
             LocalizationPack LP = new LocalizationPack();
             LP.Locale = Kingmaker.Localization.Shared.Locale.enGB;
-            foreach((string,string) tuple in enGBConditionStringsTuples)
-            {
-                LP.Strings.Add(tuple.Item1, tuple.Item2);
-            }
 
+            using (StreamReader sr = new StreamReader( modEntry.Path + "Localization\\enGB.json"))
+            {
+                string json = sr.ReadToEnd();
+                var dict = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
+                foreach(KeyValuePair<string,string> pair in dict)
+                {
+                    LP.Strings.Add(pair.Key + "Title", pair.Key + " Condition");
+                    LP.Strings.Add(pair.Key + "Description", pair.Value);
+                }
+            }
             LocalizationManager.CurrentPack.AddStrings(LP);
+
         }
     }
 }
--- EncyclopediaExpanded/Main.cs
+++ EncyclopediaExpanded/Main.cs
@@ -21,6 +21,7 @@ namespace EncyclopediaExpanded
         public static bool Load(UnityModManager.ModEntry modEntry)
         {
             Logger = modEntry.Logger;
+            Localization.modEntry = modEntry;
             Harmony harmony = new Harmony(modEntry.Info.Id);
             harmony.PatchAll(Assembly.GetExecutingAssembly());
             return true;
@@ -39,7 +40,38 @@ namespace EncyclopediaExpanded
                 BlueprintGuid combatModsGuid = BlueprintGuid.Parse("d214e02b38add2c48a334a12c1e83003");
                 BlueprintEncyclopediaPage combatMods = (BlueprintEncyclopediaPage)__instance.Load(combatModsGuid);
 
-                PageCreator.CreateConditionPage("Shaken",combatMods, ref __instance);
+                String[] conditions =
+                {
+                    "Ability damage",
+                    "Ability drain",
+                    "Bleeding",
+                    "Blinded",
+                    "Charmed",
+                    "Confused",
+                    "Difficult terrain",
+                    "Frightened",
+                    "Grappled",
+                    "Helpless",
+                    "Invisible",
+                    "Madness",
+                    "Nauseated",
+                    "Panicked",
+                    "Prone",
+                    "Paralyzed",
+                    "Petrified",
+                    "Shaken",
+                    "Sickened",
+                    "Sleeping",
+                    "Slowed",
+                    "Staggered",
+                    "Stunned",
+                    "Weakened"
+                };
+                foreach (string condition in conditions)
+                {
+                    PageCreator.CreateConditionPage(condition, combatMods, ref __instance);
+                }
+                //PageCreator.CreateConditionPage("Shaken",combatMods, ref __instance);
 
                 Main.Logger.Log("BlueprintsCache finish");
             }
